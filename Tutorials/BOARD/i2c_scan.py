845bf313e6fcfb0738e0233066fcb11d90e3f1e4
lupin3000 <steffenlorenz@hotmail.de>
2023-02-04 14:50:29 +0100

    tiny changes

--- Tutorials/BOARD/i2c_scan.py
+++ Tutorials/BOARD/i2c_scan.py
@@ -2,13 +2,13 @@ from micropython import const
 from machine import I2C, Pin
 
 
-# define constant
+# define constants
 I2C_SDA_PIN = const(21)
 I2C_SCL_PIN = const(22)
 I2C_FREQUENCY = const(400000)
 
 # create I2C object
-i2c = I2C(0, sda=Pin(I2C_SDA_PIN), scl=Pin(I2C_SCL_PIN), freq=I2C_FREQUENCY)
+i2c = I2C(0, scl=Pin(I2C_SCL_PIN), sda=Pin(I2C_SDA_PIN), freq=I2C_FREQUENCY)
 
 print('[Info] Scanning the I2C bus')
 devices = i2c.scan()
@@ -17,5 +17,7 @@ device_count = len(devices)
 if device_count == 0:
     print('[INFO] No I2C device found')
 else:
+    print(f'[INFO] {device_count} device(s) found')
+
     for device in devices:
         print(f'[INFO] Decimal address: {device} Hex address: {hex(device)}')
